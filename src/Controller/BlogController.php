531cdb34e0c0a3d45c1c13df91c6cbb6bad9411e
ahcene-kadi <you@example.com>
2021-10-12 17:24:28 +0200

    création article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,36 +2,84 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
-    public function home(): Response 
+    public function home(): Response
     {
-        return $this->render('blog/home.html.twig');
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenue sur le blog",
+            'age'   => 3
+        ]);
     }
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
      */
-    public function show(): Response 
+    public function form(Article $article = null, Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('blog/show.html.twig');
+        if(!$article) {
+            $article = new Article();
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&&$form->isValid())
+        {
+            if(!$article->getId())
+            {
+                $article->setCreatedAt(new \DateTime());
+            }
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show',
+             ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
     }
 
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article): Response
+    {
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }
 }
